3de792b7ab043468b90ea511d45537399f3020bd
loping151 <wangkailing151@gmail.com>
2024-03-14 16:06:08 +0800

    add ai search

--- analysis/searcher.py
+++ analysis/searcher.py
@@ -2,8 +2,10 @@
 
 import webbrowser
 import traceback
+import string
+import random
 
-def debugger(func):
+def stacksearch(func):
     def wrapper(*args, **kwargs):
         try:
             return func(*args, **kwargs)
@@ -27,9 +29,23 @@ def canyoubaidu(func):
     return wrapper
 
 
+def aisearch151(func):
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except Exception:
+            rid = ''.join(random.choices(string.ascii_letters + string.digits, k=30))
+            error_msg = traceback.format_exc().splitlines()[-1]
+            print(f"Error: {error_msg}")
+            webbrowser.open(f"http://search.loping151.com/ui/search.html?q={error_msg}&rid={rid}")
+            raise
+    return wrapper
+
+
 if __name__ == "__main__":
     @canyoubaidu
-    @debugger
+    @stacksearch
+    @aisearch151
     def faulty_function():
         return 1 / 0
 
